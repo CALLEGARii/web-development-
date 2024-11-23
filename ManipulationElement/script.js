f866fab8086221b5e639181500ee681d7e076c93
Rafael Callegari De Oliveira <137407464+CALLEGARii@users.noreply.github.com>
2024-11-23 19:45:59 -0300

    Add files via upload

--- ManipulationElement/script.js
+++ ManipulationElement/script.js
@@ -1,17 +1,26 @@
 var nome = prompt("Qual seu nome?");
-/*
-if (nome === null) {
-  alert("você cancelou o Prompt");
-} else if (
-  confirm(" Olá " + nome + "\n" + "\n" + "você sabe programar em Javascript?")
-) {
-  alert("Que ótimo " + nome + ", você irá aprender ainda mais!");
-} else {
-  alert("Não tem problema " + nome + ", você irá aprender agora!");
-}
-*/
+
 document.body.innerHTML += `<p id = "large">Seja Bem Vindo <b>${nome}</b></p>`;
 document.querySelector("#large").style.fontSize = "25px";
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.new').addEventListener('click', function(event) {
+    event.preventDefault();
+    const p = document.createElement('p');
+    p.textContent = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+    document.querySelector('.center').appendChild(p);
+  });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  document.querySelector('.clean').addEventListener('click', function(event) {
+    event.preventDefault();
+    const paragraphs = document.querySelectorAll('.center p');
+    paragraphs.forEach(function(paragraph) {
+      paragraph.remove();
+    });
+  });
+});
 
 
 
