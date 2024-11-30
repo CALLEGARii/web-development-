af9a74b0457ece8d0905ca509151f8e8f293681b
Rafael Callegari De Oliveira <137407464+CALLEGARii@users.noreply.github.com>
2024-11-30 12:30:18 -0300

    Add files via upload

---
+++ Mostrar-Esconder/script.js
@@ -0,0 +1,7 @@
+document.getElementById("mostrar").addEventListener("click", function () {
+  document.getElementById("mydiv").style.display = "block";
+});
+
+document.getElementById("esconder").addEventListener("click", function () {
+  document.getElementById("mydiv").style.display = "none";
+});
